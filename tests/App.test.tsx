19733d870a1946045ba9f82b818e749294c5e704
ShivangiAsthanaAccolite <shivangi.asthana@accolitedigital.com>
2024-08-27 12:46:28 +0530

    test.extend example

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 
 import App from "../src/App";
+import { expect } from "vitest";
+import { myTest } from "./my-test.js";
 
 describe("App", () => {
   it("renders the App component", () => {
@@ -14,4 +16,20 @@ describe("App", () => {
     // Create a snapshot of the component's output
     expect(asFragment()).toMatchSnapshot();
   });
+});
+
+myTest("add items to todos", ({ todos }) => {
+  expect(todos.length).toBe(3);
+
+  todos.push(4);
+  expect(todos.length).toBe(4);
+});
+
+myTest("move items from todos to archive", ({ todos, archive }) => {
+  expect(todos.length).toBe(3);
+  expect(archive.length).toBe(0);
+
+  archive.push(todos.pop());
+  expect(todos.length).toBe(2);
+  expect(archive.length).toBe(1);
 });
